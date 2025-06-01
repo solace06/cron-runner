f3aada1208dfeb99f93f2f7247471e460b7531e4
solace06 <khushi.sharma061101@gmail.com>
2025-06-01 20:02:24 +0530

    added model for returning error

--- job/models.go
+++ job/models.go
@@ -1,7 +1,15 @@
 package job
 
 type UserRegister struct {
-	UserName  string
-	Email     string
-	Password  string
+	UserName string `json:"username"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+type ProblemDetails struct {
+	Type     string `json:"type"`
+	Title    string `json:"title"`
+	Status   int    `json: status`
+	Details  string `json:"details,omitempty"`
+	Instance string `json:"instance,omitempty"`
 }
