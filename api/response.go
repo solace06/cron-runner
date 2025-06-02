429799d03ec47136239cedbf4ee8d55349f31e60
solace06 <khushi.sharma061101@gmail.com>
2025-06-02 14:45:48 +0530

    add functions for standard outputs

--- api/response.go
+++ api/response.go
@@ -1,1 +1,18 @@
 package api
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+func WriteProblem(w http.ResponseWriter, pd *ProblemDetails){
+	w.Header().Set("Content-Type","application/problem+json")
+	w.WriteHeader(pd.Status)
+	json.NewEncoder(w).Encode(pd)
+}
+
+func WriteResponse(w http.ResponseWriter, status int, data interface{}){
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	json.NewEncoder(w).Encode(data)
+}
