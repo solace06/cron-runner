c937340379be94547beb2e229968c12a8dd6fb47
solace06 <khushi.sharma061101@gmail.com>
2025-07-01 20:38:25 +0530

    added model for response

--- job/models.go
+++ job/models.go
@@ -1,7 +1,11 @@
 package job
 
 type UserRegister struct {
-	UserName string `json:"username"`
-	Email    string `json:"email"`
-	Password string `json:"password"`
+	UserName string `json:"username" validate:"required"`
+	Email    string `json:"email" validate:"required"`
+	Password string `json:"password" validate:"required"`
+}
+
+type Response struct {
+	Message string `json:"message"`
 }
